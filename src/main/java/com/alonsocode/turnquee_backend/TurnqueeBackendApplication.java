ab7ca1664c26892d52948b5b272d828bfc114baf
uavargas23 <uavargas23@gmail.com>
2025-10-06 20:09:47 -0500

    Creando la estructura básica del proyecto,

---
+++ src/main/java/com/alonsocode/turnquee_backend/TurnqueeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alonsocode.turnquee_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnqueeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnqueeBackendApplication.class, args);
+	}
+
+}
