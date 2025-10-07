1ee41a678a2b14a49950791c38ddf013503a66ff
uavargas23 <uavargas23@gmail.com>
2025-10-06 20:37:53 -0500

    Creando la entidad usuarios

---
+++ src/main/java/com/alonsocode/turnquee_backend/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.alonsocode.turnquee_backend.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios",indexes = {@Index(columnList = "documentoIdentidad",unique = true)} ) // Indice único para el documento de identidad
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// Genera un ID autoincremental
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String documentoIdentidad;
+    private String correo;
+
+}
